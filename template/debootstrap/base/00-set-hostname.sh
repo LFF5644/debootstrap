207b92f34c73096869cfa8a670eb88ecc358f033
LFF5644 (ExoneI5Tower) <meerschweinchen5644@gmail.com>
2026-06-14 17:42:46 +0200

    optimised added hosts and upgraded log feture xD

--- template/debootstrap/base/00-set-hostname.sh
+++ template/debootstrap/base/00-set-hostname.sh
@@ -1,5 +1,13 @@
+# template/root$ ln -s ../etc/skel/.bashrc .bashrc
+
+mkdir -p /debootstrap/backup
+cp -v /etc/hostname /debootstrap/backup/hostname.bak
+cp -v /etc/hosts /debootstrap/backup/hosts.bak
 
 log "set hostname to $NEW_HOSTNAME"
 # Sets New System Hostname
 echo "$NEW_HOSTNAME" > /etc/hostname
 hostname "$NEW_HOSTNAME"
+
+echo -e "# LOCALHOST\n127.0.0.1	$NEW_HOSTNAME" > /etc/hosts
+cat /debootstrap/backup/hosts.bak >> /etc/hosts
