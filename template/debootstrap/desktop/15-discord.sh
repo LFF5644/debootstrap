5afd0c0dc0ea4a053a1daee5c3a55da42c0e83d4
LFF5644 (ExoneI5Tower) <meerschweinchen5644@gmail.com>
2026-06-17 23:28:26 +0200

    #3 fix

--- template/debootstrap/desktop/15-discord.sh
+++ template/debootstrap/desktop/15-discord.sh
@@ -1,7 +1,7 @@
 if contains_package "/debootstrap/desktop/15-packages-custom.txt" "discord"; then
 	log "I KNOW ONLY SOMETIMES WORKING, because of CERT ERR!"
 	log "Installing Discord..."
-	http_proxy="" wget -O "/tmp/discord.deb" "https://discordapp.com/api/download?platform=linux&format=deb"
+	wget --no-check-certificate -O "/tmp/discord.deb" "https://discordapp.com/api/download?platform=linux&format=deb"
 	apt install /tmp/discord.deb -y
 	rm /tmp/discord.deb
 fi
