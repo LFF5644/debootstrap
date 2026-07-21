ce8fe7225eb1a30e21eec5587d7f14773a448bdc
LFF5644 (ExoneI5Tower) <meerschweinchen5644@gmail.com>
2026-07-21 21:36:51 +0200

    changed var names

--- debootstrap.sh
+++ debootstrap.sh
@@ -11,32 +11,34 @@ echo include packages: $PACKAGES_INCLUDE
 echo exclude packages: $PACKAGES_EXCLUDE
 http_proxy=$proxy
 https_proxy=$proxy
-time sudo -E debootstrap --arch=amd64 --include="$PACKAGES_INCLUDE" --exclude="$PACKAGES_EXCLUDE" --variant=minbase trixie $target_root http://deb.debian.org/debian
+time sudo -E debootstrap --arch=amd64 --include="$PACKAGES_INCLUDE" --exclude="$PACKAGES_EXCLUDE" --variant=minbase trixie $ROOT_INSTALL http://deb.debian.org/debian
 unset PACKAGES_INCLUDE
 unset PACKAGES_EXCLUDE
 unset http_proxy
 unset https_proxy
 
 echo strating with template copy...
-time sudo cp -rvTL template $target_root
+time sudo cp -rvTL template $ROOT_INSTALL
 # -L (dereference): Zwingt cp, symbolischen Links zu folgen. Statt der Verknüpfung selbst wird die echte Datei oder der echte Ordner kopiert, auf den der Link zeigt.
 # -T: Sorgt wie gewünscht dafür, dass kein neuer Unterordner im Ziel entsteht.
 
 echo mounting / binding to chroot...
-sudo mount --bind /dev $target_root/dev
-sudo mount --bind /proc $target_root/proc
-sudo mount --bind /sys $target_root/sys
-sudo mkdir -p $target_root/dev/pts
-sudo mount -t devpts devpts $target_root/dev/pts
+sudo mount --bind /dev $ROOT_INSTALL/dev
+sudo mount --bind /proc $ROOT_INSTALL/proc
+sudo mount --bind /sys $ROOT_INSTALL/sys
+sudo mount --bind /run $ROOT_INSTALL/run
+sudo mkdir -p $ROOT_INSTALL/dev/pts
+sudo mount -t devpts devpts $ROOT_INSTALL/dev/pts
 
 echo now ruinning in shroot...
-time sudo chroot $target_root bash /debootstrap/install.sh
+time sudo chroot $ROOT_INSTALL bash /debootstrap/install.sh
 
 echo finishing! now u cat use the terminal.
-sudo chroot $target_root bash
+sudo chroot $ROOT_INSTALL bash
 
 echo unmounting / unbinding ...
-sudo umount $target_root/dev/pts
-sudo umount $target_root/dev
-sudo umount $target_root/proc
-sudo umount $target_root/sys
+sudo umount $ROOT_INSTALL/dev/pts
+sudo umount $ROOT_INSTALL/dev
+sudo umount $ROOT_INSTALL/proc
+sudo umount $ROOT_INSTALL/sys
+sudo umount $ROOT_INSTALL/run
--- squashfs.sh
+++ squashfs.sh
@@ -2,27 +2,27 @@ export target_root=/tmp/debootstrap
 export target_boot=/media/lff/BOOT
 export target_device="/dev/sdf"
 
-mkdir -p $target_root/debootstrap $target_boot
-sudo cp -rvT template/debootstrap/squashfs.sh $target_root/debootstrap/squashfs.sh
+mkdir -p $ROOT_INSTALL/debootstrap $target_boot
+sudo cp -rvT template/debootstrap/squashfs.sh $ROOT_INSTALL/debootstrap/squashfs.sh
 
 echo mounting / binding to chroot...
-sudo mount --bind /dev $target_root/dev
-sudo mount --bind /proc $target_root/proc
-sudo mount --bind /sys $target_root/sys
-sudo mount --bind $target_boot $target_root/boot
+sudo mount --bind /dev $ROOT_INSTALL/dev
+sudo mount --bind /proc $ROOT_INSTALL/proc
+sudo mount --bind /sys $ROOT_INSTALL/sys
+sudo mount --bind $target_boot $ROOT_INSTALL/boot
 
 echo now ruinning in shroot...
-time sudo chroot $target_root bash /debootstrap/squashfs.sh $target_device
+time sudo chroot $ROOT_INSTALL bash /debootstrap/squashfs.sh $target_device
 
 echo finishing! now u cat use the terminal.
-sudo chroot $target_root bash
+sudo chroot $ROOT_INSTALL bash
 
 echo unmounting / unbinding ...
-sudo umount $target_root/dev
-sudo umount $target_root/proc
-sudo umount $target_root/sys
-sudo umount $target_root/boot
+sudo umount $ROOT_INSTALL/dev
+sudo umount $ROOT_INSTALL/proc
+sudo umount $ROOT_INSTALL/sys
+sudo umount $ROOT_INSTALL/boot
 
-time sudo mksquashfs $target_root filesystem.squashfs -e boot -comp zstd -Xcompression-level 19 -mem-percent 50 -info -progress
+time sudo mksquashfs $ROOT_INSTALL filesystem.squashfs -e boot -comp zstd -Xcompression-level 19 -mem-percent 50 -info -progress
 
 # sudo mksquashfs / /mnt/filesystem-current.squashfs -e boot proc sys dev run tmp mnt media lost+found -comp zstd -Xcompression-level 19 -mem-percent 50 -info -progress
